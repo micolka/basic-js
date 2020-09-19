a7070322e6f0ba491cd250d23d4a51c25438f798
micolka <mic_la@tut.by>
2020-09-19 11:13:58 +0300

    fix: what-season tests pass corect

--- src/what-season.js
+++ src/what-season.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   if (!date) return "Unable to determine the time of year!";
-  if (!(date instanceof Date)) throw new Error("Invalid argument");
+  if (toString.call(date) !== '[object Date]') throw new Error("Invalid argument");
 
   let month = "";
 
